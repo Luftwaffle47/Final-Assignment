9ad51035576a4df9aea2437aa94b36307e939978
Luftwaffle47 <60714356+Luftwaffle47@users.noreply.github.com>
2021-12-14 01:56:39 -0500

    Update bmi_server.java

--- bmi_server.java
+++ bmi_server.java
@@ -19,11 +19,11 @@ import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 public class bmi_server
-{//Instructions: Make sure you run this file first before the bmi_client file as otherwise the client will not recieve a calculation properly
+{//Instructions: Make sure you run this file first before the bmi_client file as otherwise the client will not recieve a calculation or classification properly
 	public JFrame mainFrame; static TextArea TxtArea = null;
   
 bmi_server()
-{
+{//lines 27-40 check the GUI of the clientside 
 	mainFrame = new JFrame("Server");
 	mainFrame.setSize(600,600);
 	mainFrame.setLayout(new GridLayout(3, 1));
@@ -67,14 +67,14 @@ public static void main(String args[])
 
 			w = w * 1;//Weight in Kilograms
 			h = h * 1;//Height in meters
-			Double sq = (h * h);
+			Double sq = (h * h); //General equation for BMI is weight/(height * height)
 			DecimalFormat df = new DecimalFormat(".##");
-			Double result = Double.parseDouble(df.format(w/sq));
+			Double result = Double.parseDouble(df.format(w/sq)); //This is the calculation for measuring BMI
 			String message = "";
-			if(result > 30){message = "Obese";}
+			if(result > 30){message = "Obese";}//These values and classifications are based on what is provided by the CDC
 			if(result > 25 && result < 29.9){message = "Overweight";}
 			if(result > 18.5 && result < 24.9){message = "Healthy Weight";}
-			if(result < 18.5){message = "Underweight";}
+			if(result < 18.5){message = "Underweight";} //these will send a classification to the client based on the calculated BMI
 
 			OutputStream os = socket.getOutputStream();
 			OutputStreamWriter osw = new OutputStreamWriter(os);
